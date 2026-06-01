008e756b98d64c5c907cc55ef285795e5a0d98d3
S10267556 <s10267556@connect.np.edu.sg>
2026-06-01 11:49:37 +0800

    Set up + first feature demo
    Set up the scenes + environment to work on the project
    
    Finished the demo of the first activity
    -Players are able to add 'toys' to a pile, helping daffodil 'clean up'
    -Message is displayed once the activity is done

---
+++ Assets/Scripts/PileGrow.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+
+public class PileGrow : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject nextPile; //Assign the next pile to be activated when the toys are added to the current pile
+
+    [SerializeField]
+    private GameObject finishedMessage; //Assign a Canvas to display a congratulatory message when the activity is finished
+
+    [SerializeField]
+    private bool isLastPile; //Check to determine whether the current pile is the last one
+
+    /// <summary>
+    /// When a toy enters the collider of the pile, it gets destroyed, and depending on whether it is the last pile or not, the pile will either grow, or a congratulatory message will be displayed
+    /// </summary>
+    /// <param name="collider"></param>
+    void OnTriggerEnter(Collider collider)
+    {
+        if (collider.CompareTag("Toy") && !isLastPile) //if a toy enters the pile and it is not the last pile
+        {
+            Destroy(collider.gameObject);
+            gameObject.SetActive(false);
+            nextPile.SetActive(true);
+            Debug.Log("pile grows");
+        }
+        else //displays the finished message once a toy enters the last pile
+        {
+            Destroy(collider.gameObject);
+            finishedMessage.SetActive(true);
+            Debug.Log("pile done");
+        }
+    }
+}
